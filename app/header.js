f1b4cc2bd65b1e11fd361aa4d808ae607c32967e
Catalina Insussarry <cinsussarry@itba.edu.ar>
2024-08-12 09:52:44 -0300

    mi primer commit

---
+++ app/header.js
@@ -0,0 +1,7 @@
+function Header({children}){
+    
+    return (
+        <header>{children}</header>
+    );
+}
+export default Header;
---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Header from './header';
+
+export default function Page(){
+    return(
+        <section>
+        <Header>
+            <h1>Lee!</h1>
+        </Header>
+        </section>
+    );
+}
